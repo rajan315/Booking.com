fc4695e226986b501786cc2717c57a22fe2554c6
rajan315 <rajanpgohel31.com>
2024-07-17 19:22:28 +0530

    api-client-fix

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,5 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
+import { HotelType } from "../../backend/src/shared/types";
 
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
@@ -76,14 +77,14 @@ export const addMyHotel = async (hotelFormData: FormData) => {
   return response.json();
 };
 
-export const fetchMyHotels = async() => {
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
   const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
-    credentials: "include"
+    credentials: "include",
   });
 
-  if(!response.ok){
-    throw new Error("Error fetching hotels")
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
   }
 
   return response.json();
-}
+};
